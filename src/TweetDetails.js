ff237e420c3472a91555de7771c78b88b4d15825
Franz <franz.hanschmidt@posteo.de>
2021-06-10 18:04:29 +0200

    basic routing set up

--- src/TweetDetails.js
+++ src/TweetDetails.js
@@ -1,3 +1,14 @@
-export default function TweetDetails() {
-  return <>{/* // display detailled info on a single tweet */}</>;
+import { useParams } from "react-router-dom";
+
+export default function TweetDetails({ tweets }) {
+  const { id } = useParams();
+  const tweet = tweets.filter((tweet) => tweet.id == id)[0];
+  return (
+    <>
+      {/* // display detailled info on a single tweet */}
+      {/* Franz' code for testing routing below ,can be deleted */}
+      <p>Here are details of tweet with id {tweet.id} </p>
+      {/* Franz' code for testing routing above, can be deleted */}
+    </>
+  );
 }
