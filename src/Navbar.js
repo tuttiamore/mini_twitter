5685dbc68f4ed3bd0f08352e185a152f6571b394
GueldenBayar <g.bayar@hotmail.de>
2021-06-10 22:09:35 +0200

    navbar/searchbar changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,24 @@ export default function Navbar() {
   return (
     <>
       {/* Logo on the left */}
+
+
       {/*Search bar -> filter through the tweets in state "tweets" and display component "SearchResults"  */}
+      const SearchBar = () => (
+        <form>
+          <label>
+            <span className="visually-hidden">Search tweets</span>
+          </label>
+          <input 
+            type="text"
+            id="search"
+            placeholder="Search tweets"
+            name=""
+            />
+          <button type="submit">Search</button>
+        </form>
+      )
+
       {/* User icon -> display userInfo on click */}
       {/* https://getbootstrap.com/docs/5.0/components/navbar/ */}
     </>
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,6 +2,24 @@ export default function SearchResults() {
   return (
     <>
       {/* filter tweets according to some search query and display only those tweets */}
+      <div>
+        <ul>
+          {tweets.map((tweet) => (
+            <li key={tweet.id}>{tweet.name}</li>
+          ))}
+          </ul>
+      </div>
+      
+      const filterTweets = (tweets, query) => {
+        if(!query) {
+          return tweets;
+      }
+      
+      return tweets.filter((tweet) => {
+          const tweetName = tweet.name.toLowerCase();
+          return tweetName.includes(query);
+      });
+    };
       {/*  */}
     </>
   );
