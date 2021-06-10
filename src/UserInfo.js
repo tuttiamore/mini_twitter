f360326d347172a60c79c451010f887e88d72456
Jugesh <jugeshkumar0208@gmail.com>
2021-06-10 23:07:54 +0200

    worked on userInfo and TweetDetail

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,3 +1,11 @@
-export default function UserInfo() {
-  return <>{/* Display user info */}</>;
+import UserInfoDetail from "./UserInfoDetail";
+
+export default function UserInfo({ userInfo }) {
+  return (
+    <section>
+      {userInfo.map((userinfo) => {
+        return <UserInfoDetail userinfo={userinfo}></UserInfoDetail>;
+      })}
+    </section>
+  );
 }
